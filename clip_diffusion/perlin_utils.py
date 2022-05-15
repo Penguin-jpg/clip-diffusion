f034d14a3b48679a19f2218593060661b6af9267
Penguin.jpg <sky026912@gmail.com>
2022-05-15 13:36:54 +0800

    forgot to use power parameter

--- clip_diffusion/perlin_utils.py
+++ clip_diffusion/perlin_utils.py
@@ -64,7 +64,7 @@ def perlin_ms(octaves, width, height, grayscale, device=device):
         # 對每個生成的square都要做
         for oct in octaves:
             # 0~1之間的值，決定東西會多快減小
-            p = perlin(oct_width, oct_height, scale, device)
+            p = perlin(5, oct_width, oct_height, scale, device)
             out_array[i] += p * oct
             scale //= 2
             # 以每次兩倍的頻率上升
