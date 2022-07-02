1e08895176ac3a038ce8eff54021972daac24917
Penguin.jpg <sky026912@gmail.com>
2022-07-02 15:43:46 +0800

    separate clip_client and dataset

--- clip_diffusion/instance_segmentation/dataset.py
+++ clip_diffusion/instance_segmentation/dataset.py
@@ -1,72 +1,8 @@
 import os
-from IPython.display import Image, display
-from clip_retrieval.clip_client import ClipClient, Modality
 from img2dataset import download
 from mmdet.datasets import build_dataset
 from labelme2coco import get_coco_from_labelme_folder, save_json
 from clip_diffusion.utils.dir_utils import make_dir
-
-
-def _show_result(result):
-    id, caption, url, similarity = (
-        result["id"],
-        result["caption"],
-        result["url"],
-        result["similarity"],
-    )
-    print(f"id: {id}")
-    print(f"caption: {caption}")
-    print(f"url: {url}")
-    print(f"similarity: {similarity}")
-    display(Image(url=url, unconfined=True))
-
-
-def create_clip_client(
-    backend_url="https://knn5.laion.ai/knn-service",
-    indice_name="laion5B",
-    aesthetic_score=9,
-    aesthetic_weight=0.5,
-    modality=Modality.IMAGE,
-    num_images=500,
-):
-    """
-    建立Clip retrieval client
-    """
-
-    return ClipClient(
-        url=backend_url,
-        indice_name=indice_name,
-        aesthetic_score=aesthetic_score,
-        aesthetic_weight=aesthetic_weight,
-        modality=modality,
-        num_images=num_images,
-    )
-
-
-def query_by_text(client, text, show_first_result=True):
-    """
-    透過文字進行query
-    """
-
-    results = client.query(text=text)
-
-    if show_first_result:
-        _show_result(results[0])
-
-    return results
-
-
-def query_by_image(client, image_url, show_first_result=True):
-    """
-    透過圖片進行query
-    """
-
-    results = client.query(image=image_url)
-
-    if show_first_result:
-        _show_result(results[0])
-
-    return results
 
 
 def images_to_dataset(
