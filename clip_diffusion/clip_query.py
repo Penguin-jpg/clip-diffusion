37dc6c88633835892e9cce64ff2b685a35c45220
Penguin.jpg <sky026912@gmail.com>
2022-07-04 17:55:58 +0800

    將query_by_text和query_by_image整合成get_queries、將images_to_datasets改名為download_images_from_urls並提供更多自訂選項

--- clip_diffusion/clip_query.py
+++ clip_diffusion/clip_query.py
@@ -57,17 +57,34 @@ def create_clip_client(
         aesthetic_weight=aesthetic_weight,
         modality=modality,
         num_images=num_images,
-    )
+    ).query
 
 
-def query_by_text(
-    client, text, show_first_result=True, to_json=False, json_file_path=None
+def get_queries(
+    client,
+    text=None,
+    image=None,
+    num_results=100,
+    show_first_result=True,
+    to_json=False,
+    json_file_path=None,
 ):
     """
-    透過文字進行query
+    透過文字或圖片進行query
     """
 
-    results = client.query(text=text)
+    if num_results < 0:
+        print("number of results cannot be zero")
+        return
+
+    results = client.query(text=text, image=image)
+
+    if num_results > len(results):
+        print(
+            "excceeds max number of results! automatically shorten to match max length"
+        )
+    else:
+        results = results[:num_results]
 
     if show_first_result:
         _show_result(results[0])
@@ -78,50 +95,56 @@ def query_by_text(
     return results
 
 
-def query_by_image(
-    client, image_url, show_first_result=True, to_json=False, json_file_path=None
-):
-    """
-    透過圖片進行query
-    """
-
-    results = client.query(image=image_url)
-
-    if show_first_result:
-        _show_result(results[0])
-
-    if to_json:
-        _results_to_json(results, json_file_path)
-
-    return results
-
-
-def images_to_dataset(
-    url_list,
+def download_images_from_urls(
+    url_file_path,
     output_dir,
     num_processes=1,
     num_threads=256,
     image_size=256,
-    output_format="files",
+    resize_mode="border",
+    encode_format="png",
+    encode_quality=0,
     input_format="json",
-    num_samples_per_shard=1000,
+    output_format="files",
+    num_samples_per_shard=10000,
+    timeout=10,
+    num_retires=0,
     distributor="multiprocessing",
 ):
     """
-    將圖片轉為dataset
+    透過指定的url_file下載圖片
+    url_file_path: 儲存要下載的url的檔案
+    output_dir: 下載圖片的儲存位置
+    num_processes: 處理的process數量
+    num_threads: 處理的thread數量
+    image_size: 下載的圖片會resize到這個大小
+    resize_mode: resize的方式(no, border, keep_ratio, center_crop)
+    encode_format: 輸出的圖片格式(jpg, png, webp)
+    encode_quality: encode的品質，範圍從0~100(當使用png時應為0~9)，越高圖像壓縮越多
+    input_format: url_file_path的格式(txt, csv, tsv.gz, json, parquet)
+    output_format: 下載的圖片要如何儲存(files, webdataset, parquet, tfrecord,dummy)
+    num_samples_per_shard: 每個subfolder最多能存幾張圖片
+    timeout: 下載最多等幾秒
+    num_retires: 當timeout時重試的次數
+    distributor: 分散下載的方式(multiprocessing, pyspark)
     """
 
     make_dir(output_dir, remove_old=True)
 
     # 下載圖片
     download(
-        url_list=url_list,
+        url_list=url_file_path,
         output_folder=output_dir,
         processes_count=num_processes,
         thread_count=num_threads,
         image_size=image_size,
-        output_format=output_format,
+        resize_mode=resize_mode,
+        encode_format=encode_format,
+        encode_quality=encode_quality,
         input_format=input_format,
+        output_format=output_format,
         number_sample_per_shard=num_samples_per_shard,
+        timeout=timeout,
+        retries=num_retires,
         distributor=distributor,
     )
