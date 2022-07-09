5f5a52b191e43444127a5ec56dae78c1fc0c6c1a
Penguin.jpg <sky026912@gmail.com>
2022-07-09 11:41:44 +0800

    將L2 norm分離出來以增加可讀性

--- clip_diffusion/text2image/loss.py
+++ clip_diffusion/text2image/loss.py
@@ -3,12 +3,20 @@ from torch.nn import functional as F
 # 來源：https://colab.research.google.com/drive/1QBsaDAZv8np29FPbvjffbE1eytoJcsgA#scrollTo=YHOj78Yvx8jP
 
 
+def L2_norm(input, dim=-1):
+    """
+    對input的dim做L2 norm
+    """
+
+    return F.normalize(input, dim=dim)
+
+
 def spherical_dist_loss(x, y):
     """計算大圓距離"""
 
     # 求x, y最後一個維度的L2 norm(歐基里德距離)
-    x = F.normalize(x, dim=-1)
-    y = F.normalize(y, dim=-1)
+    x = L2_norm(x, dim=-1)
+    y = L2_norm(y, dim=-1)
     # 套入公式(作者有稍微對原式做修改)
     return (x - y).norm(dim=-1).div(2).arcsin().pow(2).mul(2)
 
