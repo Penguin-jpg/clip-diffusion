5bbe74d6c0549889471d6c30bde0b06c2d89833e
Penguin.jpg <sky026912@gmail.com>
2022-07-18 17:37:30 +0800

    將clip_query.py的function統整到QueryClient內、取消單一embedding，改以text+image的方式

--- clip_diffusion/clip_query.py
+++ clip_diffusion/clip_query.py
@@ -7,133 +7,143 @@ from clip_retrieval.clip_client import ClipClient, Modality
 from clip_diffusion.utils.dir_utils import make_dir
 from clip_diffusion.utils.functional import to_clip_image, tokenize, get_text_embedding, get_image_embedding
 
-_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-_model, _preprocess = clip.load("ViT-L/14", device=_device)
-_model.eval().requires_grad_(False)
 
-
-def _results_to_json(results, output_path):
+# 參考並改寫自：https://colab.research.google.com/github/rom1504/clip-retrieval/blob/master/notebook/clip-client-query-api.ipynb?hl=zh-tw#scrollTo=1YSHcuCPgHhY
+class QueryClient:
     """
-    將query的結果存成json
+    負責進行query的client
     """
 
-    if output_path:
-        dir_path = os.path.dirname(output_path)  # 取出output_path中的資料夾名稱
-        # 如果output_path包含資料夾路徑
-        if dir_path != "":
-            make_dir(dir_path)
+    def __init__(
+        self,
+        backend_url="https://knn5.laion.ai/knn-service",
+        indice_name="laion5B",
+        aesthetic_score=9,
+        aesthetic_weight=0.5,
+        modality=Modality.IMAGE,
+        num_images=500,
+    ):
+        self.client = self._create_clip_client(backend_url, indice_name, aesthetic_score, aesthetic_weight, modality, num_images)
+        self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self._model, self._preprocess = clip.load("ViT-L/14", device=self._device)
+        self._model.eval().requires_grad_(False)
 
-        with open(output_path, "w") as file:
-            import json
+    def _create_clip_client(
+        self,
+        backend_url="https://knn5.laion.ai/knn-service",
+        indice_name="laion5B",
+        aesthetic_score=9,
+        aesthetic_weight=0.5,
+        modality=Modality.IMAGE,
+        num_images=500,
+    ):
+        """
+        建立Clip retrieval client
+        """
 
-            json.dump(results, file)
-    else:
-        print("path cannot be empty")
+        return ClipClient(
+            url=backend_url,
+            indice_name=indice_name,
+            aesthetic_score=aesthetic_score,
+            aesthetic_weight=aesthetic_weight,
+            modality=modality,
+            num_images=num_images,
+        )
 
+    def _results_to_json(self, results, output_path):
+        """
+        將query的結果存成json
+        """
 
-def create_clip_client(
-    backend_url="https://knn5.laion.ai/knn-service",
-    indice_name="laion5B",
-    aesthetic_score=9,
-    aesthetic_weight=0.5,
-    modality=Modality.IMAGE,
-    num_images=500,
-):
-    """
-    建立Clip retrieval client
-    """
+        if output_path:
+            dir_path = os.path.dirname(output_path)  # 取出output_path中的資料夾名稱
+            # 如果output_path包含資料夾路徑
+            if dir_path != "":
+                make_dir(dir_path)
 
-    return ClipClient(
-        url=backend_url,
-        indice_name=indice_name,
-        aesthetic_score=aesthetic_score,
-        aesthetic_weight=aesthetic_weight,
-        modality=modality,
-        num_images=num_images,
-    )
+            with open(output_path, "w") as file:
+                import json
 
+                json.dump(results, file)
+        else:
+            print("path cannot be empty")
 
-# 參考並修改自：https://colab.research.google.com/drive/1V66mUeJbXrTuQITvJunvnWVn96FEbSI3#scrollTo=YHOj78Yvx8jP
-def _fetch_image(url):
-    if not url.startswith("http://") or not url.startswith("https://"):
-        print("not a valid url")
-        return
-    else:
-        request = requests.get(url)
-        request.raise_for_status()
-        output = io.BytesIO()
-        output.write(request.content)
-        output.seek(0)
-        return output
+    # 參考並修改自：https://colab.research.google.com/drive/1V66mUeJbXrTuQITvJunvnWVn96FEbSI3#scrollTo=YHOj78Yvx8jP
+    def _fetch_image(self, url):
+        if not url.startswith("http://") or not url.startswith("https://"):
+            print("not a valid url")
+            return
+        else:
+            request = requests.get(url)
+            request.raise_for_status()
+            output = io.BytesIO()
+            output.write(request.content)
+            output.seek(0)
+            return output
 
+    def _merge_embeddings(self, embedding1, embedding2):
+        """
+        合併兩個embedding
+        """
 
-def _get_embedding(text=None, image_url=None):
-    """
-    根據輸入決定要使用的embedding
-    """
+        merged = embedding1 + embedding2  # 兩個embedding相加
+        l2_norm = torch.norm(merged, p=2, dim=-1, keepdim=True)  # 算出相加後的L2 norm
+        return merged / l2_norm  # L2 normalize
 
-    assert text or image_url, "need to specify text or image_url"
+    def get_query_results(
+        self,
+        text=None,
+        image_url=None,
+        num_results=500,
+        to_json=False,
+        output_path=None,
+    ):
+        """
+        透過文字或圖片進行query
+        """
 
-    if text:
-        return get_text_embedding(_model, tokenize(text, _device), divided_by_norm=True)[0].tolist()
-    else:
-        image = _fetch_image(image_url)
-        return get_image_embedding(
-            _model,
-            to_clip_image(_preprocess, image, _device),
-            use_normalize=False,
-            divided_by_norm=True,
-        )[0].tolist()
+        assert num_results >= 0, "number of results cannot be zero"
 
+        # 如果text和image_url都有值就透過embedding組合
+        if text and image_url:
+            text_embedding = get_text_embedding(self._model, tokenize(text, self._device), divided_by_norm=True)[0].tolist()
+            image_embedding = get_image_embedding(
+                self._model,
+                to_clip_image(self._preprocess, self._fetch_image(image_url), self._device),
+                use_normalize=False,
+                divided_by_norm=True,
+            )[0].tolist()
+            results = self.client.query(embedding_input=self._merge_embeddings(text_embedding, image_embedding))
+        else:
+            results = self.client.query(text=text, image=image_url)
 
-def get_query_results(
-    client,
-    text=None,
-    image_url=None,
-    use_embedding=False,
-    num_results=500,
-    to_json=False,
-    output_path=None,
-):
-    """
-    透過文字或圖片進行query
-    """
+        if num_results > len(results):
+            print("excceeds max number of results! automatically shorten to match max length")
+        else:
+            results = results[:num_results]
 
-    assert num_results >= 0, "number of results cannot be zero"
+        if to_json:
+            self._results_to_json(results, output_path)
 
-    if use_embedding:
-        results = client.query(embedding_input=_get_embedding(text, image_url))
-    else:
-        results = client.query(text=text, image=image_url)
+        return results
 
-    if num_results > len(results):
-        print("excceeds max number of results! automatically shorten to match max length")
-    else:
-        results = results[:num_results]
+    def combine_results(self, results_1, results_2, num_results=1000, to_json=False, output_path=None):
+        """
+        將兩個results結合
+        """
 
-    if to_json:
-        _results_to_json(results, output_path)
+        if num_results < 0:
+            print("number of results cannot be zero")
+            return
 
-    return results
+        new_results = results_1 + results_2
 
+        if num_results > len(new_results):
+            print("excceeds max number of results! automatically shorten to match max length")
+        else:
+            new_results = new_results[:num_results]
 
-def combine_results(results_1, results_2, num_results=1000, to_json=False, output_path=None):
-    """
-    將兩個results結合
-    """
+        if to_json:
+            self._results_to_json(new_results, output_path)
 
-    if num_results < 0:
-        print("number of results cannot be zero")
-        return
-
-    new_results = results_1 + results_2
-
-    if num_results > len(new_results):
-        print("excceeds max number of results! automatically shorten to match max length")
-    else:
-        new_results = new_results[:num_results]
-
-    if to_json:
-        _results_to_json(new_results, output_path)
-
-    return new_results
+        return new_results
