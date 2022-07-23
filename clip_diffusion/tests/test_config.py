7c797ab938c10342eea5145639ebbdc1b991d023
Penguin.jpg <sky026912@gmail.com>
2022-07-23 23:17:43 +0800

    調整以符合pytest

--- clip_diffusion/tests/test_config.py
+++ clip_diffusion/tests/test_config.py
@@ -1,56 +1,38 @@
-import unittest
+import pytest
 from clip_diffusion.config import Config
 
 
-class TestConfig(unittest.TestCase):
-    def setUp(self):
-        self.config = Config()
-
-    def tearDown(self):
-        return super().tearDown()
-
-    def test_schedules(self):
-        """
-        測試schedule建立是否正常
-        """
-
-        print("testing schedules")
-
-        overview_cut_schedule = (14,) * 200 + (12,) * 200 + (4,) * 400 + (0,) * 200
-        inner_cut_schedule = (2,) * 200 + (4,) * 200 + (2,) * 400 + (12,) * 200
-        inner_cut_size_power_schedule = (5,) * 1000
-        cut_gray_portion_schedule = (0.7,) * 100 + (0.6,) * 100 + (0.45,) * 100 + (0.3,) * 100 + (0,) * 600
-
-        self.assertTupleEqual(self.config.overview_cut_schedule, overview_cut_schedule, "diff found at overview_cut_schedule")
-        self.assertTupleEqual(self.config.inner_cut_schedule, inner_cut_schedule, "diff found at inner_cut_schedule")
-        self.assertTupleEqual(
-            self.config.inner_cut_size_power_schedule,
-            inner_cut_size_power_schedule,
-            "diff found at inner_cut_size_power_schedule",
-        )
-        self.assertTupleEqual(
-            self.config.cut_gray_portion_schedule, cut_gray_portion_schedule, "diff found at cut_gray_portion_schedule"
-        )
-
-    def test_seed(self):
-        """
-        測試是否有拿到種子
-        """
-
-        print("testing seed")
-
-        self.assertIsNotNone(self.config.seed, "seed is None")
-
-    def test_chosen_clip_models(self):
-        """
-        測試chosen_clip_models是否有正常判斷
-        """
-
-        print("testing chosen clip models")
-
-        with self.assertRaises(AssertionError):
-            self.config.adjust_settings(chosen_clip_models=("ABC"))
+config = Config()
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_schedules():
+    """
+    測試schedule建立是否正常
+    """
+
+    overview_cut_schedule = (14,) * 200 + (12,) * 200 + (4,) * 400 + (0,) * 200
+    inner_cut_schedule = (2,) * 200 + (4,) * 200 + (2,) * 400 + (12,) * 200
+    inner_cut_size_power_schedule = (5,) * 1000
+    cut_gray_portion_schedule = (0.7,) * 100 + (0.6,) * 100 + (0.45,) * 100 + (0.3,) * 100 + (0,) * 600
+
+    assert config.overview_cut_schedule == overview_cut_schedule, "diff found at overview_cut_schedule"
+    assert config.inner_cut_schedule == inner_cut_schedule, "diff found at inner_cut_schedule"
+    assert config.inner_cut_size_power_schedule == inner_cut_size_power_schedule, "diff found at inner_cut_size_power_schedule"
+    assert config.cut_gray_portion_schedule == cut_gray_portion_schedule, "diff found at cut_gray_portion_schedule"
+
+
+def test_seed():
+    """
+    測試是否有拿到種子
+    """
+
+    assert config.seed is not None, "seed is None"
+
+
+def test_chosen_clip_models():
+    """
+    測試chosen_clip_models是否有正常判斷
+    """
+
+    with pytest.raises(AssertionError):
+        config.adjust_settings(chosen_clip_models=("ABC"))
--- clip_diffusion/tests/test_models.py
+++ clip_diffusion/tests/test_models.py
@@ -1,4 +1,3 @@
-import unittest
 import torch
 from torch import nn
 from realesrgan.utils import RealESRGANer
@@ -11,62 +10,50 @@ from clip_diffusion.models import (
     load_real_esrgan_upsampler,
 )
 
-
-class TestModels(unittest.TestCase):
-    def setUp(self):
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    def test_clip_models(self):
-        """
-        測試clip_models是否正常載入
-        """
-
-        print("testing clip models")
-
-        models, _ = load_clip_models_and_preprocessings(config.chosen_clip_models, self.device)
-        for model in models:
-            self.assertIsInstance(model, nn.Module)
-
-    def test_guided_diffusion_model(self):
-        """
-        測試guided_diffusion_model是否正常載入
-        """
-
-        print("testing guided diffusion model")
-
-        model, diffusion = load_guided_diffusion_model(steps=200, device=self.device)
-        self.assertIsInstance(model, nn.Module)
-
-    def test_secondary_model(self):
-        """
-        測試secondary_model是否正常載入
-        """
-
-        print("testing secondary model")
-
-        model = load_secondary_model(self.device)
-        self.assertIsInstance(model, nn.Module)
-
-    def test_latent_diffusion_model(self):
-        """
-        測試latent_diffusion_model是否正常載入
-        """
-
-        print("testing latent diffusion model")
-
-        model = load_latent_diffusion_model(self.device)
-        self.assertIsInstance(model, nn.Module)
-
-    def test_real_esrgan_upsampler(self):
-        """
-        測試real_esrgan_upsampler是否正常載入
-        """
-
-        print("testing real esrgan upsampler")
-
-        upsampler = load_real_esrgan_upsampler(self.device)
-        self.assertIsInstance(upsampler, RealESRGANer)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_clip_models():
+    """
+    測試clip_models是否正常載入
+    """
+
+    models, _ = load_clip_models_and_preprocessings(config.chosen_clip_models, device)
+    for model in models:
+        assert isinstance(model, nn.Module), "failed to loading clip model"
+
+
+def test_guided_diffusion_model():
+    """
+    測試guided_diffusion_model是否正常載入
+    """
+
+    model, _ = load_guided_diffusion_model(steps=200, device=device)
+    assert isinstance(model, nn.Module), "failed to loading guided diffusion model"
+
+
+def test_secondary_model():
+    """
+    測試secondary_model是否正常載入
+    """
+
+    model = load_secondary_model(device)
+    assert isinstance(model, nn.Module), "failed to loading secondary model"
+
+
+def test_latent_diffusion_model():
+    """
+    測試latent_diffusion_model是否正常載入
+    """
+
+    model = load_latent_diffusion_model(device)
+    assert isinstance(model, nn.Module), "failed to loading latent diffusion model"
+
+
+def test_real_esrgan_upsampler():
+    """
+    測試real_esrgan_upsampler是否正常載入
+    """
+
+    upsampler = load_real_esrgan_upsampler(device)
+    assert isinstance(upsampler, RealESRGANer), "failed to loading real-esrgan upsampler"
