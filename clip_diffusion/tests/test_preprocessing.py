4d6d482c496ac6bd389d19bc5527430d8e77d53e
Penguin.jpg <sky026912@gmail.com>
2022-07-23 23:17:43 +0800

    調整以符合pytest

--- clip_diffusion/tests/test_preprocessing.py
+++ clip_diffusion/tests/test_preprocessing.py
@@ -1,35 +1,28 @@
-import unittest
 from clip_diffusion.preprocessing import _get_text_and_weight, prompts_preprocessing
 
 
-class TestPreprocessing(unittest.TestCase):
-    def test_text_and_weight(self):
-        """
-        測試文字和權重是否有正確分離
-        """
+def test_text_and_weight():
+    """
+    測試文字和權重是否有正確分離
+    """
 
-        print("testing text and weight")
-
-        text1, weight1 = _get_text_and_weight("a cute golden retriever, trending on artstation.")
-        text2, weight2 = _get_text_and_weight("a robot dog:5.")
-        self.assertEqual(text1, "a cute golden retriever, trending on artstation.")
-        self.assertEqual(weight1, 1.0)
-        self.assertEqual(text2, "a robot dog")
-        self.assertEqual(weight2, 5.0)
-
-    def test_prompt_translation(self):
-        """
-        測試prompt_preprocessing
-        """
-
-        print("testing prompt translation")
-
-        english_prompts = ["A castle on the hill."]
-        chinese_prompts = ["山上的一座城堡。"]
-
-        self.assertListEqual(english_prompts, prompts_preprocessing(english_prompts))
-        self.assertListEqual(english_prompts, prompts_preprocessing(chinese_prompts))
+    text1, weight1 = _get_text_and_weight("a cute golden retriever, trending on artstation.")
+    text2, weight2 = _get_text_and_weight("a robot dog:5.")
+    assert text1 == "a cute golden retriever, trending on artstation.", "failed to split text from prompt without weight"
+    assert weight1 == 1.0, "failed to automatically append weight"
+    assert text2 == "a robot dog", "failed to split text from prompt with weight"
+    assert weight2 == 5.0, "failed to split weight from prompt with weight"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_prompt_translation():
+    """
+    測試prompt_preprocessing
+    """
+
+    print("testing prompt translation")
+
+    english_prompts = ["A castle on the hill."]
+    chinese_prompts = ["山上的一座城堡。"]
+
+    assert english_prompts == prompts_preprocessing(english_prompts), "failed to translate english prompt"
+    assert prompts_preprocessing(chinese_prompts) == english_prompts, "failed to translate chinese prompt"
