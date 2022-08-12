7334a4bf2bdd0313ab64de85c2e76a92c09c5569
Penguin.jpg <sky026912@gmail.com>
2022-08-12 21:41:21 +0800

    fix

--- clip_diffusion/utils/functional.py
+++ clip_diffusion/utils/functional.py
@@ -12,7 +12,7 @@ from IPython import display
 from PIL import ImageFont, ImageDraw
 
 # Clip用到的preprocess
-CLIP_NORMALIZE = (T.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711)),)
+CLIP_NORMALIZE = T.Normalize(mean=(0.48145466, 0.4578275, 0.40821073), std=(0.26862954, 0.26130258, 0.27577711))
 CLIP_PREPROCESS = T.Compose(
     [
         T.Resize(224, T.InterpolationMode.LANCZOS),
