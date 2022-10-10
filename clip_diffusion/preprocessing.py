b8e4aa7354f28995e1e367f6c9cb186a668ff6df
Penguin.jpg <sky026912@gmail.com>
2022-10-10 21:45:13 +0800

    調整create_init_image_tensor和create_mask_tensor

--- clip_diffusion/preprocessing.py
+++ clip_diffusion/preprocessing.py
@@ -29,8 +29,12 @@ def create_init_image_tensor(init_image, resize_shape, device=None):
     init_image_tensor = None
     # 如果初始圖片不為空
     if init_image is not None:
-        # 將anvil傳來的image bytes轉成Pillow Image
-        image = get_image_from_bytes(init_image.get_bytes()).convert("RGB")
+        if isinstance(init_image, str):
+            # 如果是字串就直接開啟
+            image = Image.open(init_image).convert("RGB")
+        else:
+            # 將anvil傳來的image bytes轉成Pillow Image
+            image = get_image_from_bytes(init_image.get_bytes()).convert("RGB")
         image = image.resize(resize_shape, Image.LANCZOS)  # 調整圖片大小
         image_tensor = image_to_tensor(image, device).unsqueeze(0)  # 轉tensor並擴增一個batch_size維度
         init_image_tensor = normalize_image_neg_one_to_one(image_tensor)  # 將範圍normalize到[-1, 1]
@@ -41,7 +45,10 @@ def create_mask_tensor(mask_image, resize_shape, device=None):
     """建立mask tensor"""
     mask_tensor = None
     if mask_image is not None:
-        mask = get_image_from_bytes(mask_image.get_bytes())
+        if isinstance(mask_image, str):
+            mask = Image.open(mask_image)
+        else:
+            mask = get_image_from_bytes(mask_image.get_bytes())
         # 建立一個白色的背景(因為anvil傳來的圖片會去背，如果直接二值化會導致全部變成黑色)
         background = Image.new("RGB", mask.size, "WHITE")
         background.paste(mask, box=(0, 0), mask=mask)  # 將mask貼到background上
